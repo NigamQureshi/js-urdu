d0bbd8c487d9f7866409a9dd586186a686b453aa
NigamQureshi <145166133+NigamQureshi@users.noreply.github.com>
2023-09-17 21:32:50 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+let age = 25
+let name = "Nigam"
+let maritalStatus = true
+let state = null
+
+/*PRIMITIVE DATATYPES
+numbers => 2 to power 53
+bigInt
+string => ""
+boolean => True/False
+null => standalone value
+undefined => variable decalared but not defined
+symbol => unique
+
+OBJECTS DATATYPES
+*/
+
+console.log(typeof state);
+
+//null is an object and it is considered as a language making error
