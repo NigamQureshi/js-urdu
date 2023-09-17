e0f36acd38e24331002df85fed57cdbba287a9a0
NigamQureshi <145166133+NigamQureshi@users.noreply.github.com>
2023-09-17 21:00:59 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let acountEmail ="nigam@gmail.com"
+var accountPassword ="1233445"
+accountCity ="hyderabad"
+let accountState;
+
+// accountId =54321
+
+console.log(accountId);
+console.table([accountId, acountEmail , accountPassword, accountCity, accountState]);
