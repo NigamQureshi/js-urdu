8fac32c6f4fa46d4c89223b515100a8ca04d9c51
NigamQureshi <145166133+NigamQureshi@users.noreply.github.com>
2023-09-18 22:33:28 +0000

    type conversion

---
+++ 01_basics/03_typeConversion.js
@@ -0,0 +1,40 @@
+/*let score = "Nigam"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+//now lets do type conversion. for type conver we write Number in capital
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/* "33" => 33
+    "3abc" => Nan
+    true => 1 , false => 0 
+*/
+
+//type conversion in boolean 
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+//type conversion in string
+
+let isLoggedIn = false
+
+let stringIsLoggedIn = String(isLoggedIn);
+
+console.log(typeof stringIsLoggedIn);
+console.log(stringIsLoggedIn);
+
+
+
+
+
